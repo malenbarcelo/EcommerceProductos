54f272a2372a1ed494a2b38e7bf7163dfe40f252
EmiMena <emilianomena2019@hotmail.com>
2022-10-16 20:20:27 -0300

    home
    Terminado el modelo del home

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express=require('express')
 const path = require('path')
 const app = express()
-const publicPath =  path.resolve('public')
+const publicPath =  path.resolve(__dirname, './public')
 const APP_PORT = 3000
 
 app.use(express.static(publicPath)) //le decimos que queremos la carpeta como un recurso de archivos estáticos
 
 app.listen(APP_PORT,() => console.log("Servidor corriendo en puerto " + APP_PORT))
 
-app.get('/',(req,res) =>res.sendFile(path.resolve('./views/layout.html')))
+app.get('/',(req,res) =>res.sendFile(path.resolve('./views/index.html')))
 app.get('/product-detail',(req,res) =>res.sendFile(path.resolve('./views/productDetail.html')))
 app.get('/product-cart',(req,res) =>res.sendFile(path.resolve('./views/prductCart.html')))
 app.get('/register',(req,res) =>res.sendFile(path.resolve('./views/register.html')))
