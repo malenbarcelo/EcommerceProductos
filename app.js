ca9999a1e83f29d6e99013900df9cc9be1ea2615
malenbarcelo <barcelomalen@gmail.com>
2022-10-24 20:14:42 -0300

    retro sprint 2 + algunos cambios

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.listen(APP_PORT,() => console.log("Servidor corriendo en puerto " + APP_PORT
 
 app.get('/',(req,res) =>res.sendFile(path.resolve('./views/index.html')))
 app.get('/product-detail',(req,res) =>res.sendFile(path.resolve('./views/productDetail.html')))
-app.get('/product-cart',(req,res) =>res.sendFile(path.resolve('./views/prductCart.html')))
+app.get('/productCart',(req,res) =>res.sendFile(path.resolve('./views/productCart.html')))
 app.get('/register',(req,res) =>res.sendFile(path.resolve('./views/register.html')))
 app.get('/login',(req,res) =>res.sendFile(path.resolve('./views/login.html')))
 app.get('/header-y-footer',(req,res) =>res.sendFile(path.resolve('./views/header-y-footer.html')))
---
+++ retro-sprint2.md
@@ -0,0 +1,20 @@
+# Retro Grupo 3 - SiO2 - Sprint 2
+
+## 1. Comenzar a hacer
+- Plasmar lineamientos generales por escrito
+- Ser más proactivos a la hora de resolver imprevistos
+- Coordinar dos reuniones semanales para lograr una comunicación más fluida
+
+## 2. Hacer más
+- Comunicación más fluida con los profesores
+- Reuniones más frecuentes
+
+## 3. Continuar haciendo
+- Continuar con la comunicación fluida dentro del grupo
+
+## 4. Hacer menos
+- Dejar pasar mucho tiempo entre que nos entregan las consignas y comenzamos a trabajarlas
+
+## 5. Dejar de hacer
+- Dejar las cosas para último momento
+
