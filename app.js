6d38fc6d66bad42b30b583e29602591b9db070c0
malenbarcelo <barcelomalen@gmail.com>
2022-11-14 18:56:12 -0300

    json productos + json usuarios + página de productos

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 const mainRoutes = require('./routes/mainRoutes.js')
 const productsRoutes = require('./routes/productsRoutes.js')
 const usersRoutes = require('./routes/usersRoutes.js')
-const publicPath =  path.resolve(__dirname, './public')
+const publicPath =  path.resolve('./public')
 const APP_PORT = 3000
 
 app.use(express.static(publicPath)) //le decimos que queremos la carpeta como un recurso de archivos estáticos
@@ -14,5 +14,5 @@ app.set('view engine','ejs')
 app.listen(APP_PORT,() => console.log("Servidor corriendo en puerto " + APP_PORT))
 
 app.use('/',mainRoutes)
-app.use('/',productsRoutes)
+app.use('/products',productsRoutes)
 app.use('/',usersRoutes)
--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,7 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.resolve('./data/productsJSON.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
 const productsController = {
-    detail: (req,res) => {
-        res.render('products/productDetail',{title:'Detalle'})
-    },
+    allProducts: (req,res) => {
+        res.render('products/products',{title:'Productos',products:products})
+    },    
+    productDetail: (req,res) => {
+        let productId = req.params.productId
+        let selectedProduct = products.find(product => product.id == productId)
+        res.render('products/productDetail',{title:'Detalle del producto',selectedProduct})
+        
+        
+    },    
     cart: (req,res) => {
         res.render('products/productCart',{title:'Tu carrito de compras'})
     },
